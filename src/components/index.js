bf2057f556b6ccb98cb8fcb3289041cd07f00881
disha <dishakatiyar123@gmail.com>
2023-01-22 17:13:51 +0530

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+export {default as Article} from './article/Article';
+export {default as Brand} from './brand/Brand';
+export {default as CTA} from './cta/CTA';
+export {default as Feature} from './feature/Feature';
+export {default as Navbar} from './navbar/Navbar';
+
+
+// Note : 
+//     1. components is created for "smaller components",
+//        container is created for "bigger components"
+//     2. 'index.js' file create inside a component can be directly accessed by
+//        just folder name.(i.e. index.js in components)
+//     3. CTA -> Click To Action
